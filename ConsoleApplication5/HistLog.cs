838bc97782c9da00536d3663d25104a9b6d44d09
cameron <cameron.tasmania@gmail.com>
2016-08-04 21:29:07 +1000

    New HistLog class. Not yet implemented. End of Day.

---
+++ ConsoleApplication5/HistLog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Next_Game
+{
+    public enum ActorEvent {Born, Died, Married}
+
+    //
+    //tracks historical Information & Events ---
+    //
+
+    //Base class
+    class HistLog
+    {
+        private static int eventIndex = 1;
+        public int eventID { get; }
+        public int Year { get; set; }
+        public int Turn { get; set; } = 0; //pre-game start events don't need this, hence '0'
+        private List<string> description; //A log entry can have multiple lines of text if needed
+
+        public HistLog()
+        {
+            description = new List<string>();
+            eventID = eventIndex++;
+        }
+    }
+
+    //Actor event log
+    class HLogActor : HistLog
+    {
+        public ActorEvent Type { get; set; }
+        private List<int> listOfActors;
+        private List<int> listOfLocs;
+        private List<int> listOfHouses;
+        private List<int> listOfItems;
+        
+        public HLogActor()
+        {
+            listOfActors = new List<int>();
+            listOfLocs = new List<int>();
+            listOfHouses = new List<int>();
+            listOfItems = new List<int>();
+        } 
+    }
+}
