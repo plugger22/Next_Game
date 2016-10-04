1dfe0b1793ac46660d65f68be3d2f82e36dd207f
cameron <cameron.tasmania@gmail.com>
2016-10-04 13:46:49 +1100

    Added event.cs

---
+++ ConsoleApplication5/Game World/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Next_Game
+{
+    /// <summary>
+    /// Base Event class
+    /// </summary>
+    class Event
+    {
+        private static int eventIndex = 1;
+        public int EventID { get; }
+        public string Name { get; set; }
+        public int TempID { get; set; }
+
+
+        public Event(int tempID, string name)
+        {
+            EventID = eventIndex++;
+            this.TempID = tempID;
+            this.Name = name;
+            //debug
+            Console.WriteLine("EventID {0], {1}, TempID {2}", EventID, Name, TempID);
+        }
+    }
+}
