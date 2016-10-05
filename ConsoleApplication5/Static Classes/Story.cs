1c18a8ee2079e50cc61dbd07059bee16e662dcd2
cameron <cameron.tasmania@gmail.com>
2016-10-05 12:11:32 +1100

    minor changes to Story.cs

--- ConsoleApplication5/Static Classes/Story.cs
+++ ConsoleApplication5/Static Classes/Story.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace Next_Game
 {
+
+    public enum StoryGeneral {Good, Neutral, Bad}
+    public enum StorySpecific {Tricky}
+
     /// <summary>
     /// Story AI class that manages the game world
     /// </summary>
     public class Story
     {
-        List<int> listOfActiveGeoClusters;
+        List<int> listOfActiveGeoClusters; //clusters that have a road through them
 
         public Story()
         {
