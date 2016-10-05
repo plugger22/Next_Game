8735062c3c5b12b886a3c0a58e6d1b7bb64afd5d
cameron <cameron.tasmania@gmail.com>
2016-10-05 16:08:42 +1100

    Working on Generic follower type events.

--- ConsoleApplication5/Game World/Event.cs
+++ ConsoleApplication5/Game World/Event.cs
@@ -17,6 +17,7 @@ namespace Next_Game
         public int EventID { get; }
         public string Name { get; set; }
         public int TempID { get; set; }
+        public bool Active { get; set; } = true; //can only be used if active
         public EventFrequency Frequency { get; set; }
         public ArcType AppliesTo { get; set; } = ArcType.None;
         public ArcGeo GeoType { get; set; } = ArcGeo.None;
@@ -45,10 +46,16 @@ namespace Next_Game
 
 
     /// <summary>
-    /// derived base class for Generic Events
+    /// derived base class for Generic Events (followers)
     /// </summary>
     public class EventGeneric : Event
     {
+        public string EventText { get; set; }
+        public string SuccessText { get; set; } //text to display if follower succeeds test
+        public string FailText { get; set; } //text to display if follower fails test
+        public TraitType Trait { get; set; } //trait type to test against
+        public int Delay { get; set; } //# of days delayed if fail test
+
 
         /// <summary>
         /// pass through constructor for subclasses
@@ -57,7 +64,9 @@ namespace Next_Game
         /// <param name="name"></param>
         /// <param name="frequency"></param>
         public EventGeneric(int tempID, string name, EventFrequency frequency) : base(tempID, name, frequency)
-        { }
+        {
+            
+        }
     }
 
     /// <summary>
