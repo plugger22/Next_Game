3fe28556ae332ea978698575004fe774aed33d69
cameron <cameron.tasmania@gmail.com>
2016-10-17 17:04:53 +1100

    Improvements to Option & Outcome classes.

--- ConsoleApplication5/Event_System/Option.cs
+++ ConsoleApplication5/Event_System/Option.cs
@@ -14,9 +14,9 @@ namespace Next_Game.Event_System
     public class Option
     {
         public int OptionID { get; set; }
-        public string Text { get; set; } //main text
-        public string Tooltip { get; set; } 
-        private List<string> listOfReplies; //texts for outcomes 0, 1 etc.
+        
+        public string ReplyGood { get; set; } //text for good outcome
+        public string ReplyBad { get; set; } //text for bad outcome
         private List<Outcome> listGoodOutcomes; //All possible outcomes if success or chosen
         private List<Outcome> listBadOutcomes; //All possible outcomes if fail or ignored
         private Dictionary<int, OpCondition> dictOfTriggers; //trigger conditions, if any, that must be met for the option to be active
@@ -24,13 +24,59 @@ namespace Next_Game.Event_System
 
         public Option()
         {
-            listOfReplies = new List<string>();
             listGoodOutcomes = new List<Outcome>();
             listBadOutcomes = new List<Outcome>();
             dictOfTriggers = new Dictionary<int, OpCondition>();
         }
 
-        
+        /// <summary>
+        /// add good outcome
+        /// </summary>
+        /// <param name="effect"></param>
+        internal void SetGoodOutcome(Outcome effect)
+        {
+            if (effect != null)
+            { listGoodOutcomes.Add(effect); }
+            else { Game.SetError(new Error(68, "Invalid input (Good Outcome) in Option.cs")); }
+        }
+
+        /// <summary>
+        /// add bad outcome
+        /// </summary>
+        /// <param name="effect"></param>
+        internal void SetBadOutcome(Outcome effect)
+        {
+            if (effect != null)
+            { listBadOutcomes.Add(effect); }
+            else { Game.SetError(new Error(68, "Invalid input (Bad Outcome) in Option.cs")); }
+        }
+
+        internal List<Outcome> GetGoodOutcomes()
+        { return listGoodOutcomes; }
+
+        internal List<Outcome> GetBadOutcomes()
+        { return listBadOutcomes; }
+
+        /// <summary>
+        /// set up a trigger for the option (won't show unless condition met)
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="condition"></param>
+        internal void AddTrigger(int data, OpCondition condition)
+        {
+            if (data > 0 && condition != OpCondition.None)
+            {
+                //validate data input
+                switch (condition)
+                {
+                    case OpCondition.Trait:
+
+                        break;
+                }
+            }
+            else { Game.SetError(new Error(68, "Invalid input (Trigger) in Option.cs")); }
+        }
+
     }
 
     /// <summary>
@@ -38,8 +84,9 @@ namespace Next_Game.Event_System
     /// </summary>
     public class OptionAuto : Option
     {
-        public TraitType TraitCondition { get; set; } //trait type that the option uses as a succeed/fail test
-
+        public TraitType Trait { get; set; } //trait type that the option uses as a succeed/fail test
+        public string Text { get; set; } //main text
+        public string Tooltip { get; set; }
     }
 
 
