612bdb03d9637f458c25e48f9b242b9ca32f1345
cameron <cameron.tasmania@gmail.com>
2016-11-09 18:01:00 +1100

    Implement Card.cs basic framework.

---
+++ ConsoleApplication5/Event_System/Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Next_Game.Event_System
+{
+
+    public enum CardCategory { None, Conflict, Item, Secret, Companion }
+    public enum ConflictType { None, Skill, Trait, Situation, Companion, Item, Supporter }
+    public enum CardType { None, Good, Bad, Neutral}
+
+    /// <summary>
+    /// Base class
+    /// </summary>
+    class Card
+    {
+        public CardType Type { get; set; }
+        public CardCategory Category { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    /// <summary>
+    /// Conflict Cards
+    /// </summary>
+    class CardConflict : Card
+    {
+        public int Effect { get; set; } //eg. 1X, 2X, etc.
+        public string Special { get; set; }
+        public ConflictType Conflict_Type { get; set; }
+    }
+
+
+}
