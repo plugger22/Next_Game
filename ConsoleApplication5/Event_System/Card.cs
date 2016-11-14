b851f9c4b7d4af8d47bb894d5bf2f4d9145bc7a3
cameron <cameron.tasmania@gmail.com>
2016-11-14 17:41:33 +1100

    Reconsidering Situation.cs -> Better to morph necessary functionality into Cards.cs

--- ConsoleApplication5/Event_System/Card.cs
+++ ConsoleApplication5/Event_System/Card.cs
@@ -16,10 +16,11 @@ namespace Next_Game.Event_System
     /// </summary>
     class Card
     {
-        public CardType Type { get; set; }
+        public CardType Type { get; set; } //General type
         public CardCategory Category { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        
 
         public Card()
         { }
@@ -40,6 +41,9 @@ namespace Next_Game.Event_System
         public int Effect { get; set; } //eg. 1X, 2X, etc.
         public string Special { get; set; }
         public CardConflict Conflict_Type { get; set; }
+        public CardType TypeAttack { get; set; } //how a situation card is treated if defender chooses an Attack strategy
+        public CardType TypeBalanced { get; set; } //how a situation card is treated if defender chooses a balanced strategy
+        public CardType TypeDefend { get; set; } //how a situation card is treated if defender chooses a defend strategy
 
         public Card_Conflict()
         { }
@@ -52,6 +56,10 @@ namespace Next_Game.Event_System
             Category = CardCategory.Conflict;
             Conflict_Type = conflictType;
             this.Effect = effect;
+            //default values of None for Types (indicates normal use, no restrictions)
+            TypeAttack = CardType.None;
+            TypeBalanced = CardType.None;
+            TypeDefend = CardType.None;
         }
 
     }
--- ConsoleApplication5/Event_System/Situation.cs
+++ ConsoleApplication5/Event_System/Situation.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace Next_Game.Event_System
 {
+    ///NOTE -> CURRENTLY NOT USED
+
     /// <summary>
     /// Card Conflict Situations
     /// </summary>
@@ -14,9 +16,9 @@ namespace Next_Game.Event_System
         public string Description { get; set; }
         public int Effect { get; set; } //eg. 1X, 2X
         public int Level { get; set; } //How many cards (random weighted determination)
-        public CardType TypeAttack { get; set; } //how situation is treated if player chooses an Attack strategy
-        public CardType TypeBalanced { get; set; } //how situation is treated if player chooses a balanced strategy
-        public CardType TypeDefend { get; set; } //how situation is treated if player chooses a defend strategy
+        public CardType TypeAttack { get; set; } //how situation is treated if defender chooses an Attack strategy
+        public CardType TypeBalanced { get; set; } //how situation is treated if defender chooses a balanced strategy
+        public CardType TypeDefend { get; set; } //how situation is treated if defender chooses a defend strategy
 
         //default constructor
         public Situation(string description, CardType attack, CardType balanced, CardType defend, int level = 0)
@@ -27,12 +29,7 @@ namespace Next_Game.Event_System
             TypeDefend = defend;
             //default effect of 1X
             Effect = 1;
-            if (level == 0)
-            {
-                //randomly determine effect (70% - 1, 16% - 2, 8% - 3, 4% - 4, 2% - 5)
-               
-            }
-            else { this.Level = level; }
+            this.Level = level;
         }
 
     }
